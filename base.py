6609ddcbbb8ac260ca8987beca86546c76e4e6a8
maboh-kd <blastcherry@icloud.com>
2025-10-01 14:44:14 +0900

    Delete base.py

--- base.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-from ..types import TrendItem
-
-class TrendSource:
-    name = "base"
-    def fetch(self, limit: int = 5) -> List[TrendItem]:
-        raise NotImplementedError
